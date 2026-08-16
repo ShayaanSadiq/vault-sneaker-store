065edd0b5a693126b6e4a3d0f30ee576ab0e6432
ShayaanSadiq <shayaansadiq1402@gmail.com>
2026-08-16 21:04:46 +0530

    Add a Spring Boot API project.

---
+++ backend/src/main/java/com/vault/store/VaultStoreApplication.java
@@ -0,0 +1,12 @@
+package com.vault.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaultStoreApplication.class, args);
+    }
+}
